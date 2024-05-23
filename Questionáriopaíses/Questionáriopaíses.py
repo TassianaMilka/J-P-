a6d77f931391f38c87010865b38e0b335c5ab92a
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-05-23 18:20:36 -0300

    1

---
+++ Questionáriopaíses/Questionáriopaíses.py
@@ -0,0 +1,127 @@
+#Os usuários de um aplicativo tiveram o acesso do resultado com sete perguntas sobre 
+#países que é necessário aparecer o false ou true durante a compilação sem o if e else.
+
+
+Brasil=1
+Argentina=1
+França=2
+Canada=3
+Angola=4
+Japao=5
+Indonesia=6
+Chile=7
+Panama=8
+Costa_Rica=8
+Inglaterra=9
+China=10
+Estados_Unidos=11
+Cuba=12
+Espanha=13
+Malasia=14
+Malta=15
+Lituania=15
+Peru=16
+Holanda=17
+Canada=18
+Mexico=18
+Polonia=19
+Taiwan=20
+Argelia=21
+Libia=21
+Portugal=22
+Tailandia=23
+Uruguai=24
+India=25
+Argelia=26
+Noruega=27
+Russia=28
+Italia=29
+Bolivia=30
+Nepal=31
+Sri_Land=31
+Eslovaquia=33
+Mongolia=34
+Nova_Zelandia=35
+Ilha_Salomao=35
+Belgica=36
+Paraguai=37
+
+
+
+print('1-Qual países fazem parte da América do Sul?')
+print('Brasil e Argentina')
+print(Brasil==Argentina)
+True
+print('França e  Canadá')
+print(França==Argentina)
+False
+print('Angola e Japão')
+print(Angola==Japao)
+False
+print('\n')
+print('2-Qual países fazem parte da América Central?')
+print('Indonésia e Chile')
+print(Indonesia==Chile)
+False
+print('Panamá e  Costa Rica')
+print(Panama==Costa_Rica)
+True
+print('Inglaterra e China')
+print(Inglaterra==China)
+False
+print('\n')
+print('3-Qual países fazem parte da Europa?')
+print('Estados Unidos e Cuba')
+print(Estados_Unidos==Cuba)
+False
+print('Espanha e  Malásia ')
+print(Espanha==Malasia)
+False
+print('Malta e Lituânia ')
+print(Malta==Lituania)
+True
+print('\n')
+print('4-Qual países fazem parte da América do Norte')
+print('Peru e Holanda')
+print(Peru==Holanda)
+False
+print('Canadá e  México ')
+print(Canada==Mexico)
+True
+print('Polônia e Taiwan')
+print(Polonia==Taiwan)
+False
+print('\n')
+print('5-Qual países fazem parte da África? ')
+print('Argélia e Líbia ')
+print(Argelia==Libia)
+True
+print('Portugal e  Tailândia ')
+print(Portugal==Tailandia)
+False 
+print('Uruguai e India')
+print(Uruguai==India)
+False
+print('\n')
+print('6-Qual países fazem parte da Ásia?')
+print('Noruega e Rússia')
+print(Noruega==Russia)
+False
+print('Itália e Bolívia ')
+print(Italia==Bolivia)
+False 
+print('Nepal e Sri land')
+print(Nepal==Sri_Land)
+True
+print('\n')
+
+print('7-Quais países fazem parte da Oceania? ')
+print('Eslováquia e Mongólia ')
+print(Eslovaquia==Mongolia)
+False
+print('Nova Zelândia e  Ilha  Salomão ')
+print(Nova_Zelandia==Ilha_Salomao)
+True
+print('Bélgica e Paraguai ')
+print(Belgica==Paraguai)
+False
