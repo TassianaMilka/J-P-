bb65c536b295901fe1ea1d9ab1fb01f7a133aa52
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-06-11 14:33:19 -0300

    1

---
+++ Java/Hello/Hello.java
@@ -0,0 +1,28 @@
+
+package hello;
+
+   //Programando em Java(1) or Programming in Java(1).
+  //1-Crie  um  programa com a frase do Hello word em três linhas como o exemplo a baixo.
+// ------------------------------------------------------------------------------------------------------------
+//                                                 Hello Word
+//                                                 Hello Word 
+//                                                 Hello Word
+//---------------------------------------------------------------------------------------------------------------
+
+public class Hello {//PrimeiroPrograma  e  //Bloco de Entrada
+	public static void main(String[] args) { //Bloco de Entrada
+	
+  	System.out.println("----------------------------");	
+	System.out.println("       Hello Word"+"\n"); 	
+	System.out.println("       Hello Word"+"\n");
+	System.out.println("       Hello Word"+"\n");
+        System.out.println("----------------------------");	
+	
+	}	//Bloco de Saída.
+	
+} // Bloco de Saída.
+
+
+    
+    
+
