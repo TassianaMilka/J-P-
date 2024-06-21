c4660a86fdbbeb525ebdfcd03ac4b61da77ccfab
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-06-20 23:27:57 -0300

    19

--- Java/Objetonomeclassemulher/Objetonomeclassemulher.java
+++ Java/Objetonomeclassemulher/Objetonomeclassemulher.java
@@ -1,1 +1,39 @@
+//Programando em Java(19) or Programming in Java(19).
 
+//Realiza uma classe com o nome mulher definidos em public void o 
+falar,pensar e andar com mensagem parclassificar.
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Main {
+    //Nome da classe
+    class mulher{
+ 
+    public String nome;
+    public int idade;
+    public String cordecabelo;
+
+       
+    public void falar(){
+        //Imprimir para classificar.
+        System.out.println( " A mulher contém uma voz");
+    }
+      
+   
+    public void pensar (){
+    //Imprimir para classificar.
+    System.out.println( " A mulher contém um pensamento");
+
+    }
+    public void andar (){
+    //Imprimir para classificar.
+    System.out.println( " A mulher consegue andar");
+
+    }
+
+    
+    }
+}
