56ec270e4f60f11f05f0a0a4361f5257deb3aa37
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-08-15 16:12:45 -0300

    1

---
+++ Python/Capacelulares/Capacelulares.py
@@ -0,0 +1,17 @@
+#Realiza um código que demonstra nome da loja capacelulares,que a quantidade é 50 maior e menor que 20
+# a quantidade.
+
+
+#Declaração de variáveis
+nome="capacelulares"
+quan=50
+#Imprimir a mensagem
+print("Olá somos da loja")
+print(nome)
+print("Esse contato é na finalidade de oferecer um desconto para kit de capa aos celulares")
+print('\n')
+if quan>20:
+    print("Maior a quantidade")
+
+if quan<20:
+    print("Menor a quantidade")
