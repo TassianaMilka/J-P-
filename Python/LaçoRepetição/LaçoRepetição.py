dba57bf0465efa723bfe83dd68860ca2f332a4a3
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-01-13 21:08:41 -0300

    Create LaçoRepetição.py

---
+++ Python/LaçoRepetição/LaçoRepetição.py
@@ -0,0 +1,17 @@
+#Crie um código sobre o laço de repetição que imprima vários números e realiza uma multiplicação
+
+#Imprimir a mensagem
+print("Imprimir os números e múltiplicar a ele mesmo\n")
+
+#Laço de repetição
+for i in range(4, 6):
+    
+    #Imprimir o resultado do laço de repetição
+    print(i)
+    
+    #Realização da multiplicação do mesmo número
+    num=i*i;
+    
+    #Imprimir o resultado
+    print(num)
+  
