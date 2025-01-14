d978f3ad5a96ca3108f4ee73cc2005c1cdee807d
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-01-13 21:17:32 -0300

    1

---
+++ Python/Frases/Frases.py
@@ -0,0 +1,10 @@
+#Crie uma lista de três frases e exibe na execução
+
+#Frases declaradas
+frases= ["O universo é rico em descoberta","Todas estrelas são criadas como uma poesia","O tempo é o amigo"]
+
+#Laço de repetição
+for frase in frases:
+    
+    #Imprimir todas frases
+    print(frase)
