b5be557d4de083ea1abbd30d2ef6fcd79d74cd76
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-10-04 23:33:45 -0300

    Create Enunciado.py

---
+++ Python/Funçãodef/Enunciado.py
@@ -0,0 +1,16 @@
+
+Criar vários códigos utilizando função def:
+
+-Para saber o nome e idade;
+-Depois criar sobre idade e soma com ano de 20230 para saber a quantos anos.
+
+
+-Requisitos 
+
+-Função;
+-Declaração;
+-String;
+-Soma;
+-Entrada e saída de dado;
+-if and else(estrutura/condição);
+-for(laço de repetição).
