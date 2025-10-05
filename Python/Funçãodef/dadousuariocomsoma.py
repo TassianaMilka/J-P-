32d183952f2bb8e0e926213334d653a82dd66778
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-10-04 22:47:47 -0300

    Create dadousuariocomsoma.py

---
+++ Python/Funçãodef/dadousuariocomsoma.py
@@ -0,0 +1,14 @@
+#Função
+def dado():
+  
+ 
+  soma = 22+5;
+  return soma;
+
+print(f"Dado do usuário registrado:Tassiana Milka com idade de 22 anos.\n\n")
+
+
+soma_idade = dado()
+
+
+print(f"Em 2030 vai conter uma idade de {soma_idade} anos.\n\n")
