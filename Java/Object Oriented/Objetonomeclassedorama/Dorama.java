c78a8bf07e4f9791551c51aae85629673e5c925d
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-10-19 00:08:49 -0300

    Create Dorama class with data display methods

---
+++ Java/Object Oriented/Objetonomeclassedorama/Dorama.java
@@ -0,0 +1,30 @@
+
+
+public class Dorama { //Nome da classe
+    //Declaração de variáveis
+    String nome_Primeiro;
+    int ano_Primeiro;
+    String nome_Segundo;
+    int ano_Segundo;
+
+
+
+    //Mensagens para identificar os dados
+    void dados_Primeiro() {
+        System.out.println("\n\n");
+        System.out.println("Nome do primeiro dorama:" + nome_Primeiro);
+        System.out.println("Ano de lançamento do primeiro dorama:" + ano_Primeiro);
+
+    }
+
+    void dados_Segundo() {
+        System.out.println("\n");
+        System.out.println("Nome do segundo dorama:" + nome_Segundo);
+        System.out.println("Ano de lançamento do segundo dorama:" + ano_Segundo);
+
+    }
+
+
+
+
+}
