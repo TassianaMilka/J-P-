f0b0ade0918ef982f6a58695d99859f6682ef3fe
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-11-15 16:11:08 -0300

    Create Desenho.java

---
+++ Java/Object Oriented/Objetonomeclassedesenho/Desenho.java
@@ -0,0 +1,19 @@
+public class Desenho{ //Nome da classe
+    //Declaração de variáveis
+    String nome="Dora, a Aventureira";
+    int ano= 2000;
+
+
+
+
+    //Mensagens para identificar os dados
+    void dados() {
+        System.out.println("\n\n");
+        System.out.println("Nome do Desenho Animado:" + nome);
+        System.out.println("Ano de lançamento do Desenho:" + ano);
+
+    }
+
+
+
+}
