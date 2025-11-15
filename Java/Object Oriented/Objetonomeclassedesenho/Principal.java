5abb0059dddb823768cc7efbf06fb1b0994e998f
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-11-15 16:09:57 -0300

    Create Principal.java

---
+++ Java/Object Oriented/Objetonomeclassedesenho/Principal.java
@@ -0,0 +1,18 @@
+//Realiza uma Orientação a Objeto simples o nome e ano lançamento de um desenho animado.
+
+
+
+
+public class Principal {//Nome da classe
+    public static void main(String[] args) {
+
+        //Dados sobre nome do desenho animado e ano de lançamento
+         Desenho desenho=new Desenho();
+
+        //Imprimir os dados descrito e do void na classe Desenho
+        desenho.dados();
+
+
+
+    }
+}
