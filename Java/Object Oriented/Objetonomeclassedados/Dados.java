6dc88f61194b92cf8f1917f5f50c5ccadc1ec469
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-11-15 22:17:50 -0300

    Create Dados.java

---
+++ Java/Object Oriented/Objetonomeclassedados/Dados.java
@@ -0,0 +1,20 @@
+
+public class Dados {//Noem da Classe
+   //Declaração de Variável
+   public String PromocaoP;
+   public String PromocaoS;
+   public String PromocaoT;
+
+
+
+
+
+    void dadosProduto(){
+        //Imprimir sobre descrição
+        System.out.println(PromocaoP);
+        System.out.println(PromocaoS);
+        System.out.println(PromocaoT);
+    }
+}
+
+
