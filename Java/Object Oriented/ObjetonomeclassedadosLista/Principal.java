7cb6a31416bdb4c348a22038f759e48e74e6af94
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-11-16 01:14:55 -0300

    Create Principal.java

---
+++ Java/Object Oriented/ObjetonomeclassedadosLista/Principal.java
@@ -0,0 +1,32 @@
+//Realiza uma Orientação a Objeto simples o nome e ano lançamento de um desenho animado.
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import modelos.Listadados;
+
+
+public class Principal {
+    public static void main(String[] args) {
+        //Importa a lista array
+        ArrayList<Listadados> lista = new ArrayList<>();
+
+        System.out.println("\n\n");
+
+        //Dados da lista
+        lista.add(new Listadados("Tassiana"));
+        lista.add(new Listadados("Milka"));
+
+
+        //Em ordem
+        Collections.reverseOrder();
+        //Loop para imprimir o dado da lista
+        for (Listadados listadados : lista) {
+            System.out.println(listadados.dados);
+
+
+        }
+
+ 
+    }
+}
