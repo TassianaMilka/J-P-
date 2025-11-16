20c62d04462841aedb243f492bd6077d64b800ac
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-11-16 02:18:04 -0300

    Create Dados.java

---
+++ Java/Object Oriented/Objetonomeclassedadosmetodo/modelo/Dados.java
@@ -0,0 +1,25 @@
+package modelo;
+
+public class Dados{//Nome da classe
+
+    public String nome;
+    public String musica;
+
+
+    //Método get e set
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMusica() {
+        return musica;
+    }
+
+    public void setMusica(String musica) {
+        this.musica = musica;
+    }
+}
