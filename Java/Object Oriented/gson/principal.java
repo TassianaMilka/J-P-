97a19f848aa81fcb97a5b3d656a9d7cc10fb24f9
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-11-20 08:59:53 -0300

    Create principal.java

---
+++ Java/Object Oriented/gson/principal.java
@@ -0,0 +1,17 @@
+//Cria um json com os dado país Brasil e cidade Campinas para imprimir o resultado.
+
+
+import com.google.gson.Gson;
+
+
+public class Principal {//Nome da classe
+    public static void main(String[] args) {
+
+
+        String jsondado = "{\"pais\":\"Brasil\",\"cidade\":\"Campinas\" }";
+        Gson gsonD = new Gson();
+        Dadoglap dado = gsonD.fromJson(jsondado, Dadoglap.class);
+        System.out.println(dado);
+
+    }
+}
