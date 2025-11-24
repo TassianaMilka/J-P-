7b32faff2ba627f172fb9ecc133e0ce6fc3c6166
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-11-24 17:50:47 -0300

    Create Modelo.java

---
+++ Java/Object Oriented/APIREST/modelo/Modelo.java
@@ -0,0 +1,25 @@
+package modelo;
+
+public class Modelo {
+
+    private String ResponseHeaders;
+    private String Bytes;
+
+
+    public String getResponseHeaders() {
+        return ResponseHeaders;
+    }
+
+    public void setResponseHeaders(String responseHeaders) {
+        ResponseHeaders = responseHeaders;
+    }
+
+    public String getBytes() {
+        return Bytes;
+    }
+
+    public void setBytes(String bytes) {
+        Bytes = bytes;
+    }
+
+}
